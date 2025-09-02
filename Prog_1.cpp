32fd39da049493daf860e480b9cc83bfd753b5ae
ritikubey <ritikubey84@gmail.com>
2025-09-02 16:34:01 +0530

    feature/menu added

---
+++ Prog_1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    cout<<"CPP Calc"<<endl;
+
+    int num1,num2;
+    cout<<"Enter Numbers";
+    cin>>num1>>num2;
+
+    cout<<"1. Sum \n2. Subtract \n3.Multilpication \n4.Division"<<endl;
+
+
+    cout<<"edit.shubh";
+
+    cout<<"edit.shubh2";
+
+    return 0;
+}
